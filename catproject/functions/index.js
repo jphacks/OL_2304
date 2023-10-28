4dd135d930620a32537b8fc30f0bc4deef097f63
fumi <fumiwata2000@gmail.com>
2023-10-28 22:45:08 +0900

    CatMapを書き換えました。猫選択画面を作りました。

---
+++ catproject/functions/index.js
@@ -0,0 +1,49 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+
+// メール送信の設定
+const transporter = nodemailer.createTransport({
+  service: "gmail", // 使用するメールサービス。ここではGmailを例としています。
+  auth: {
+    user: "fumiwata2000@gmail.com", // 送信元のメールアドレス
+    pass: "dnrq pqee debw wlmu", // 送信元のメールのパスワード
+  },
+});
+
+exports.sendMailOnNewCat = functions.firestore.document("cats/{catId}")
+    .onCreate((snap, context) => {
+      const mailOptions = {
+        from: "fumiwata2000@gmail.com",
+        to: "khirami733@gmail.com", // 送信先のメールアドレス
+        subject: "新しい猫のお知らせ",
+        text: "新しい猫が追加されました！",
+      };
+
+      return transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(`Failed to send mail: ${error}`);
+          return null;
+        }
+        console.log(`Sent email: ${info.response}`);
+        return null;
+      });
+    });
--- catproject/src/components/CatMap.vue
+++ catproject/src/components/CatMap.vue
@@ -1,26 +1,53 @@
 <template>
 
-    <div class="loading">
-      <div class="circle light"></div>
-      <div class="circle dark"></div>
-      <div class="branding"></div>
-    </div>
+  <div class="loading">
+    <div class="circle light"></div>
+    <div class="circle dark"></div>
+    <div class="branding"></div>
+  </div>
 
-    <div class="uploaded">
-      <form method="post">
+  <div class="uploaded">
+    <form method="post">
+      <div class="map-container">
         <center><GoogleMap></GoogleMap></center>
-        <button type="button" class="btn btn-primary btn-block btn-large" @click.prevent="gotoUploadExistCat">既存猫</button>
-        <button type="button" class="btn btn-primary btn-block btn-large" @click.prevent="gotoUploadNewCat">新規猫</button>
-        </form>
-    </div>
+      </div>
+
+      <div class="cat-card">
+        <div class="cat-images">
+          <div v-for="image in catImages" :key="image" class="cat-image-container">
+            <img :src="image" alt="猫の画像" class="cat-image"/>
+          </div>
+        </div>
+      </div>
+
+      <button type="button" class="btn btn-primary btn-block btn-large" @click.prevent="gotoUploadExistCat">既存猫</button>
+      <button type="button" class="btn btn-primary btn-block btn-large" @click.prevent="gotoUploadNewCat">新規猫</button>
+    </form>
+  </div>
+
 </template>
+
 
 <script>
 import GoogleMap from './GmapPost.vue'
+
 export default {
   name: 'CatMap',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      catImages: [
+        'https://newsatcl-pctr.c.yimg.jp/t/iwiz-yn/rpr/ishiimasumi/00259735/title-1632409672924.jpeg?exp=10800', 
+        'https://p.potaufeu.asahi.com/c713-p/picture/26747079/e6388b5c0937642c0e97515110ef835c.jpg',
+        'https://www.yhg.co.jp/taiyo33/column/wp/wp-content/uploads/2021/05/cat-4806217_1280-1024x682.jpg',
+        'https://stat.ameba.jp/user_images/20230501/19/alma-soulmate/84/57/j/o1080081015278087136.jpg',
+        'https://urbanlife.tokyo/wp-content/uploads/2020/11/201128_catcat_01.jpg',
+        'https://stat.ameba.jp/user_images/20230413/19/hironepu/86/d6/j/o2048191415269793409.jpg'
+        // 他の画像URLをここに追加
+      ]
+    };
   },
   methods: {
     gotoUploadExistCat() {
@@ -53,4 +80,32 @@ li {
 a {
   color: #42b983;
 }
+.map-container {
+  width: 100%;
+}
+
+.cat-card {
+  margin-top: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+}
+
+.cat-images {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px; /* 画像同士の間隔 */
+}
+
+.cat-image-container {
+  flex: 0 0 calc(16.666% - 20px); /* 画像を6列に分けるための設定。20pxはgapの値です */
+}
+
+.cat-image {
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+
 </style>
