a669e6ac0c430e25ff1ce7636e4f35dd42d64c37
inatoihs <sabanna.taihei@gmail.com>
2023-10-29 00:21:37 +0900

    絞り込みUI

--- catproject/src/components/HomePage.vue
+++ catproject/src/components/HomePage.vue
@@ -12,13 +12,12 @@
   <div class="gotoCatProfile">
     <button @click.prevent="gotoCatProfile">猫アイコン予定</button>
   </div>
-<div class="content-container">
-  <div class="dropdown-section">
-    <p2>各項目を選択してください</p2>
-      <!-- 猫の色 -->
-      <div class="form-group">
-        <label for="catColor" class="form-label">色: </label>
-        <select id="catColor" v-model="selectedColor" class="custom-dropdown">
+  <div class="home-page">
+    <div class="form-section">
+      <form>
+        <p>猫の色</p>
+        <select>
+          <option value="">指定なし</option>
           <option value="白">白</option>
           <option value="黒">黒</option>
           <option value="茶">茶</option>
@@ -27,22 +26,20 @@
           <option value="橙">橙</option>
           <option value="その他">その他</option>
         </select>
-      </div>
-      <!-- 猫の柄選択 -->
-      <div class="form-group">
-        <label for="catPattern" class="form-label">柄: </label>
-        <select id="catPattern" v-model="selectedPattern" class="custom-dropdown">
+
+        <p>猫の柄</p>
+        <select>
+          <option value="">指定なし</option>
           <option value="無地">無地</option>
           <option value="縦縞">縦縞</option>
           <option value="横縞">横縞</option>
           <option value="斑点">斑点</option>
           <option value="その他">その他</option>
         </select>
-      </div>
-      <!-- 種類の選択 -->
-      <div class="form-group">
-        <label for="catType" class="form-label">種類: </label>
-        <select id="catType" v-model="selectedType" class="custom-dropdown">
+
+        <p>猫の種類</p>
+        <select>
+          <option value="">指定なし</option>
           <option value="三毛">三毛</option>
           <option value="鯖">鯖</option>
           <option value="マンチカン">マンチカン</option>
@@ -52,35 +49,41 @@
           <option value="ブリティッシュショートヘア">ブリティッシュショートヘア</option>
           <option value="その他">その他</option>
         </select>
-      </div>
-      <!-- 子・大人の選択 -->
-      <div class="form-group">
-        <label for="catChildAdult" class="form-label">子猫/成猫: </label>
-        <select id="catChildAdult" v-model="selectedChildAdult" class="custom-dropdown">
+
+        <p>猫の年齢</p>
+        <select>
+          <option value="">指定なし</option>
           <option value="子猫">子猫</option>
           <option value="成猫">成猫</option>
-          <option value="分からない">分からない</option>
         </select>
-      </div>
-      <!-- 耳カットの選択 -->
-      <div class="form-group">
-        <label for="catChoker" class="form-label">耳カット: </label>
-        <select id="catEarCut" v-model="selectedEarCut" class="custom-dropdown">
+
+        <p>耳のカット</p>
+        <select>
+          <option value="">指定なし</option>
           <option value="あり">あり</option>
           <option value="なし">なし</option>
-          <option value="分からない">分からない</option>
         </select>
-      </div>
-      <!-- 首輪 -->
-      <div class="form-group">
-        <label for="catChoker" class="form-label">首輪: </label>
-        <select id="catChoker" v-model="selectedcatChoker" class="custom-dropdown">
+
+        <p>首輪</p>
+        <select>
+          <option value="">指定なし</option>
           <option value="あり">あり</option>
           <option value="なし">なし</option>
-          <option value="分からない">分からない</option>
         </select>
+
+        <button type="button" class="narrow-button">絞り込み</button>
+      </form>
+    </div>
+
+    <center>
+    <div class="map-section">
+      <div class="form-group">
+        <label for="map-container" class="map-container"></label>
+        <GoogleMap></GoogleMap>
       </div>
-      <button type="button" class="NarrowButton" @click.prevent="gotoFinishUpload">絞り込み</button>
+    </div>
+    </center>
+
     <!-- <center>
     <GoogleMap></GoogleMap>
     </center> -->
@@ -91,22 +94,13 @@
         <GoogleMap></GoogleMap>
       </div>
     </div> -->
-
-      <div class="map-section">
-        <div class="form-group">
-          <label for="map-container" class="map-container"></label>
-          <GoogleMap></GoogleMap>
-        </div>
-      </div>
-
-    <div class="gotoCatMap">
+  </div>
+  <div class="gotoCatMap">
       <form method="post">
         
         <button type="submit" class="btn btn-primary btn-block btn-large" @click.prevent="gotoCatMap">投稿</button>
       </form>
     </div>
-  </div>
-</div>
 </template>
 
 <script>
@@ -191,5 +185,17 @@ height: 400px; /* マップの高さを調整 */
 
 .map-section {
   flex: 1; /* 必要に応じて調整 */
+}
+.home-page {
+  display: flex;
+  flex-direction: row;
+}
+
+.form-section {
+  margin-right: 20px;
+}
+
+.map-section {
+  flex-grow: 1;
 }
 </style>
