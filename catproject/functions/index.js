152468372c860310cf9cf66b8232944a64ac4981
inatoihs <sabanna.taihei@gmail.com>
2023-10-29 09:18:46 +0900

    メールを送信するユーザーをデータベースの情報から決定

--- catproject/functions/index.js
+++ catproject/functions/index.js
@@ -17,6 +17,7 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+import calculateDistance from ./src/components/CSSMathClamp.vue;
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 
@@ -29,21 +30,30 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+//新しい猫が現れたとき、その猫の半径１キロメートル以内にいるユーザーにメールを送信する
 exports.sendMailOnNewCat = functions.firestore.document("TestCat/{catId}")
-    .onCreate((snap, context) => {
-      const mailOptions = {
-        from: "fumiwata2000@gmail.com",
-        to: "sabanna.taihei@gmail.com", // 送信先のメールアドレス
-        subject: "新しい猫のお知らせ",
-        text: "近所で新しい猫が発見されました！",
-      };
+    .onCreate(async (snap, context) => {
+      const catData = snap.data();
+      const catLat = catData.lat;
+      const catLng = catData.lng;
 
-      return transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error(`Failed to send mail: ${error}`);
-          return null;
-        }
-        console.log(`Sent email: ${info.response}`);
-        return null;
-      });
+      const usersSnapshot = await admin.firestore().collection("users").get();
+      const users = usersSnapshot.docs.map((doc) => doc.data());
+
+      const mailPromises = users
+        .filter((user) => {
+          const distance = calculateDistance(user.lat, user.lng, catLat, catLng);
+          return distance <= 1;
+        })
+        .map((user) => {
+          const mailOptions = {
+            from: "fumiwata2000@gmail.com",
+            to: user.email, // 送信先のメールアドレス
+            subject: "新しい猫のお知らせ",
+            text: "近所で新しい猫が発見されました！",
+          };
+          return transporter.sendMail(mailOptions);
+        });
+
+      return Promise.all(mailPromises);
     });
